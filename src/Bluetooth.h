6fb81ac9a5cd73d0d79fe315963a06cddccd958c
Travis Lane <travis.lane.osx@gmail.com>
2015-03-04 21:24:38 -0800

    add basic structure

---
+++ src/Bluetooth.h
@@ -0,0 +1,33 @@
+/**
+ * @file Bluetooth.h
+ * @brief
+ * @author Travis Lane
+ * @version
+ * @date 2015-03-04
+ */
+
+#ifndef ARDUINO_BLUETOOTH_H
+#define ARDUINO_BLUETOOTH_H
+
+class Bluetooth
+{
+private:
+  const char *name;
+  const char *pin;
+  int mode;
+
+public:
+  Bluetooth(const char *new_name = "",
+                    const char *new_pin = "", 
+										int new_mode = 0)
+	{
+		name = new_name;
+		pin = new_pin;
+		mode = new_mode;
+	}
+	
+	virtual void Setup() = 0;
+};
+
+
+#endif /* ARDUINO_BLUETOOTH_H */
