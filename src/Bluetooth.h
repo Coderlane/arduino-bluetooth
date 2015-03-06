c1183fe1af3e146037d2f004db33cb054d71b333
Travis Lane <travis.lane.osx@gmail.com>
2015-03-05 16:59:14 -0800

    inheritiing from stream

--- src/Bluetooth.h
+++ src/Bluetooth.h
@@ -9,27 +9,60 @@
 #ifndef ARDUINO_BLUETOOTH_H
 #define ARDUINO_BLUETOOTH_H
 
-#define NULL (void *)0
+#include <Stream.h>
+#include <SoftwareSerial.h>
 
-class Bluetooth
+class Bluetooth : public Stream
 {
 protected:
+  friend class SoftwareSerial;
+
   const char *name;
   const char *pin;
+
+  int rx_pin;
+  int tx_pin;
   int mode;
 
+  SoftwareSerial serial;
+
 public:
-  Bluetooth(int new_mode = 0,
+  Bluetooth(int new_rx_pin, int new_tx_pin,
+            int new_mode = 0,
             const char *new_name = NULL,
-            const char *new_pin = NULL) {
+            const char *new_pin = NULL)
+    : serial(new_rx_pin, new_tx_pin) {
+
     name = new_name;
     pin = new_pin;
     mode = new_mode;
+
+    rx_pin = new_rx_pin;
+    tx_pin = new_tx_pin;
   }
 
   virtual void Setup() = 0;
-  virtual void Send(char *data, int len) = 0;
-  virtual void Recv(char *data, int len) = 0;
+
+  int available() {
+    return serial.available();
+  }
+  int read() {
+    return serial.read();
+  }
+  int peek() {
+    return serial.peek();
+  }
+  void flush() {
+    serial.flush();
+  }
+
+  size_t write(uint8_t data) {
+    return serial.write(data);
+  }
+
+  size_t write(const uint8_t *buffer, size_t size) {
+    return serial.write(buffer, size);
+  }
 };
 
 #endif /* ARDUINO_BLUETOOTH_H */
