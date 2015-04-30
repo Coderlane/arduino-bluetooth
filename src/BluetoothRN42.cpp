0f416ac8c5e289301420a8a631546e94e336c23c
Travis Lane <travis.lane.osx@gmail.com>
2015-04-30 13:41:53 -0700

    Add rn42 device specific commands

--- src/BluetoothRN42.cpp
+++ src/BluetoothRN42.cpp
@@ -9,17 +9,28 @@
 #include "BluetoothRN42.h"
 #include "Arduino.h"
 
+const char *const BluetoothRN42::power_level_strs[] = {
+  RN42_POWER_N12_STR,
+  RN42_POWER_N8_STR ,
+  RN42_POWER_N4_STR,
+  RN42_POWER_0_STR,
+  RN42_POWER_4_STR,
+  RN42_POWER_8_STR,
+  RN42_POWER_12_STR,
+  RN42_POWER_16_STR
+};
+
 void BluetoothRN42::setup()
 {
   io->begin(115500);
   delay(500);
-  
+
   enterCommand();
   io->println("U,9600,N");
   ////io->println("SU,96");
   delay(500);
   io->begin(9600);
-  
+
   //setMode(mode);
   //setPin(pin);
   setName(name);
@@ -113,11 +124,69 @@ int BluetoothRN42::setCod(const char *new_cod)
   io->print(new_cod[1]);
   io->println(new_cod[0]);
   delay(50);
-  
+
   return Bluetooth::setCod(new_cod);
+}
+
+int BluetoothRN42::setInquryScanWindow(const char *scan_window)
+{
+	if(!inCommand) {
+		return -1;
+	}
+
+	io->print("SI,");
+	io->println(scan_window);
+	delay(50);
+
+	return 0;
+}
+
+int BluetoothRN42::setPageScanWindow(const char *scan_window)
+{
+	if(!inCommand) {
+		return -1;
+	}
+
+	io->print("SJ,");
+	io->println(scan_window);
+	delay(50);
+
+	return 0;
+}
+
+int BluetoothRN42::setBonding(bool bonding_enabled)
+{
+	if(!inCommand) {
+		return -1;
+	}
+
+	if(bonding_enabled) {
+		io->println("SX,1");
+	} else {
+		io->println("SX,0");
+	}
+	delay(50);
+
+	return 0;
+}
+
+int BluetoothRN42::setPower(enum rn42_power_e power_level)
+{
+	if(!inCommand) {
+		return -1;
+	} else if(power_level > RN42_POWER_MAX || power_level  << RN42_POWER_MIN) {
+		//ERANGE
+		return -2;
+	}
+
+	io->print("SC,");
+	io->println(power_level_strs[power_level]);
+	delay(50);
+
+	return 0;
 }
 
 bool BluetoothRN42::isConnected()
 {
-	return digitalRead(status_pin) == HIGH;
+  return digitalRead(status_pin) == HIGH;
 }
--- src/BluetoothRN42.h
+++ src/BluetoothRN42.h
@@ -11,6 +11,29 @@
 
 #include "Bluetooth.h"
 
+#define RN42_POWER_16_STR  "0010"
+#define RN42_POWER_12_STR  "000C"
+#define RN42_POWER_8_STR   "0008"
+#define RN42_POWER_4_STR   "0004"
+#define RN42_POWER_0_STR   "0000"
+#define RN42_POWER_N4_STR  "FFFC"
+#define RN42_POWER_N8_STR  "FFF8"
+#define RN42_POWER_N12_STR "FFF4"
+
+enum rn42_power_e {
+	RN42_POWER_16  = 7,
+	RN42_POWER_12  = 6,
+	RN42_POWER_8   = 5,
+	RN42_POWER_4   = 4,
+	RN42_POWER_0   = 3,
+	RN42_POWER_N4  = 2,
+	RN42_POWER_N8  = 1,
+	RN42_POWER_N12 = 0
+};
+
+#define RN42_POWER_MAX RN42_POWER_16
+#define RN42_POWER_MIN RN42_POWER_N12
+
 class BluetoothRN42 : public Bluetooth
 {
 protected:
@@ -19,6 +42,8 @@ protected:
 
   void enterCommand();
   void exitCommand();
+
+	static const char * const power_level_strs[];
 
 public:
   BluetoothRN42(int new_status_pin, int new_mode = 0,
@@ -33,7 +58,14 @@ public:
   int setPin(const char *new_pin);
   int setName(const char *new_name);
   int setCod(const char *new_cod);
+	
+	// Device Specific
+	int setInquryScanWindow(const char *scan_window);
+	int setPageScanWindow(const char *scan_window);
+	int setBonding(bool bonding_enabled);
+	int setPower(enum rn42_power_e power_level);
 
+	// Gotta implement
   virtual bool isConnected();
   virtual void setup();
 };
